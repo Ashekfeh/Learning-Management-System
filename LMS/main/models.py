cb8c027a148c21eb4a6e2920d04d5462f2a25c8c
Ashekfeh <abd.shekfeh@gmail.com>
2023-11-24 02:04:15 +0300

    .

--- LMS/main/models.py
+++ LMS/main/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from datetime import timedelta
+import humanize
 
 class Course(models.Model):
+    WEB = 'WD'
+    ACC = 'AC'
+    DM = 'DM'
+    IND = 'ID'
+
+
     categories = [
-        (1, 'web dev'),
-        (2, 'accounting'),
-        (3, 'digital marketing'),
-        (4, 'Interior Design')
+        (WEB, 'web dev'),
+        (ACC, 'accounting'),
+        (DM, 'digital marketing'),
+        (IND, 'Interior Design')
     ]
 
     ENGLISH = 'EN'
@@ -22,10 +30,31 @@ class Course(models.Model):
     title = models.CharField(max_length=200, blank=False)
     description = models.CharField(max_length=2000, blank=False)
     cover_image = models.FileField()
-    duration = models.TimeField(blank=False)
+    duration = models.DurationField(blank=False)
     category = models.CharField(max_length=2, choices=categories, blank=False)
     lang = models.CharField(
         max_length=2,
         choices=languages,
         default=ARABIC
     )
+
+    def __str__(self):
+        return f'{self.title}, {humanize.precisedelta(self.duration, minimum_unit="minutes")}'
+
+
+class UserExtension(models.Model):
+
+    first_name = models.CharField(max_length=200, blank=False)
+    last_name = models.CharField(max_length=200, blank=False)
+    email = models.EmailField(max_length=245)
+
+
+class Teacher(UserExtension):
+
+    courses = models.ManyToManyField(Course, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name.capitalize()} {self.last_name.capitalize()}'
+    
+
+
